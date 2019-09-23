16fd6390b41def46b61e921bf1600a79c7ad3f87
Kacper Ziarkowski <k.ziarkowski@microsoft.wsei.edu.pl>
2019-09-23 17:25:24 +0200

    Add project files.

---
+++ ClassLibrary1/AppLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLoggers
+{
+    public class AppLogger
+    {
+        public void Save(string path)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path).Close();
+            }
+            
+            
+        }
+    }
+}
---
+++ LogerTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using AppLoggers;
+
+namespace LogerTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            var logger = new AppLogger();
+            logger.Save(@"D:\texty.txt");
+            Console.Read();
+
+        }
+    }
+}
